b9f3da50784b0f131ef674b37e2b602576168165
svenmeijboom-ru <sven.meijboom@ru.nl>
2024-12-05 14:22:06 +0100

    .

--- dataloader.py
+++ dataloader.py
@@ -44,8 +44,12 @@ def data_load(dataset, has_v=True, has_a=True, has_t=True):
         a_feat = None
         t_feat = None
 
+    print(train_edge[:10])
+    print(user_item_dict[:10])
+
     train_edge[:, 1] += user_num
     user_item_dict = {i:[j+user_num for j in user_item_dict[i]] for i in user_item_dict.keys()}
+
     print(train_edge[:10])
     print(user_item_dict[:10])
 
