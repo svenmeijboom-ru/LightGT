c021a857f1f75a4f0e2bd9adf035958637fee878
svenmeijboom-ru <sven.meijboom@ru.nl>
2024-12-08 12:43:05 +0100

    .

--- test.py
+++ test.py
@@ -9,12 +9,24 @@ with open(dir_str+'instacart_merged.json', 'r') as file:
     user_item_dict = json.load(file)
 
 train_edge = []
+val = []
+test = []
 for k in user_item_dict.keys():
+    v = user_item_dict[k].pop()
+    t = user_item_dict[k].pop()
+    
+    val.append([int(k)] + v)
+    test.append([int(k)] + t)
+
     for i in user_item_dict[k]:
         for ii in i:
             train_edge.append([int(k), ii])
-    break
 
+#print(val)
+val_array_with_lists = np.array(val, dtype=object)
+test_array_with_lists = np.array(test, dtype=object)
 
+#print(user_item_dict)
 np.save(dir_str+'train.npy', train_edge)
-np.save(dir_str+'user_item_dict.npy', user_item_dict)
+np.save(dir_str+'val.npy', val_array_with_lists)
+np.save(dir_str+'test.npy', test_array_with_lists)
